10087bf94e54c68c4a346bd1bd3a30682f64c2ae
Grégory <greg.guillermin@gmail.com>
2017-05-31 01:09:14 +0100

    First working version.

--- slackbot_cryptocurrencies.py
+++ slackbot_cryptocurrencies.py
@@ -8,8 +8,16 @@ from KrakenAPI import KrakenPublic
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
+kraken=KrakenPublic()
+
+botID='U5KN0GHCM'
+botname='blockbot'
+
+vul=['encule', 'fdp', 'ntm', 'fuck']
+
+chans={'testbots':'G5K9U1UBT'}
+
 def createMessage():
-	kraken=KrakenPublic()
 	message="Server Time: "+kraken.getTime()+"\n\n"
 	currency=[kraken.getBitcoin(), kraken.getEther()]
 
@@ -22,6 +30,84 @@ def createMessage():
 
 	return message
 
+def message_help():
+	msg="My aim is to help people enjoying *crypto-trading* ~and fucking the world~.\n\"_Speculation is pejorative_\""
+	msg+="\nWhat can I understand for the moment:\n"
+	msg+="\n\t- *hey/hello*: welcome message"
+	msg+="\n\t- *help*: print this little help"
+	msg+="\n\t- *time*: print Time Server"
+	msg+="\n\t- *@blockbot* (only): print market informations about Bitcoin and Ehter"
+
+	return msg
+
+def message_time():
+	return "Kraken - Time Server "+kraken.getTime()
+
+def message_hello():
+	return "Hey, *Welcome* to the *Blockchain Jungle* !"
+
+def message_vulg():
+	return "Fais le malin aujourd'hui, tu le feras moins demain quand je serai *millionaire*."
+
+def word_analyze(resp):
+	msg=""
+	words=(resp['text']).split()
+
+	if ('<@'+botID+'>') in words[0]:
+		if len(words)==1:
+			words=[]
+		else:
+			words=words[1:] 
+
+	if len(words)==0:
+		msg=createMessage()
+	else:
+		for w in words:
+			if 'help' in w.lower():
+				msg=message_help()
+				break
+			elif 'hey' in w.lower():
+				msg=message_hello()
+				break
+			elif 'hello' in w.lower():
+				msg=message_hello()
+				break
+			elif 'time' in w.lower():
+				msg=message_time()
+				break
+
+	for w in vul:
+		if w in (resp['text']).lower():
+			msg=message_vulg()
+
+	if len(msg)>0:
+		sc.rtm_send_message(resp['channel'],msg)
+	else:
+		sc.rtm_send_message(resp['channel'],"Sorry, I can't understand ~yet~")
+
+def handle_message(resp):
+	in_chan=0
+
+	for k in chans:
+		if resp['channel']==chans[k]:
+			in_chan=1
+
+	if in_chan and (('<@'+botID+'>') in (resp['text']).split()):
+		word_analyze(resp)
+	elif not in_chan:
+		word_analyze(resp)
+
+def handle_response(resp):
+	if 'type' in resp:
+		if resp['type']=='error':
+			sc.rtm_send_message('G5K9U1UBT', (resp['error'])['msg'])
+		
+		#elif resp['type']=='desktop_notification' and resp['content'].find('@'+botname):
+		elif (resp['type']=='message') and (('text' and 'channel') in resp):
+			if 'reply_to' not in resp:
+				print(resp)
+				handle_message(resp)
+
 def main_program():
 	if sc.rtm_connect():
 	    while True:
@@ -29,19 +115,13 @@ def main_program():
         
 	        if len(resp)>0:
         		resp=resp[0]
-        		#print(resp)
-        	
-        		if 'type' in resp:
-	        	
-	        		if resp['type']=='desktop_notification' and resp['content'].find('@blockbot'):
-	        		
-	        			msg=createMessage()
-		        		sc.rtm_send_message(resp['channel'], msg)
-		        		# resp = (sc.rtm_read())[0]
-	        		
-	        			# while resp['ok'] is not True:
-	        			# 	sc.rtm_send_message(resp['channel'], msg)
-	        			# 	resp = (sc.rtm_read())[0]
+        		handle_response(resp)
 	        time.sleep(1)
 	else:
 	    print("Connection Failed, invalid token?")
+
+def main():
+	main_program()
+
+if __name__ == '__main__':
+	main()
